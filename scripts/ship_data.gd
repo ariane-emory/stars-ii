a510b063411f9cf5bf0a9b3d09040d531ff7ccd9
Ariane Emory <ariane.emory@protonmail.com>
2025-10-31 20:39:33 -0400

    ...

--- scripts/ship_data.gd
+++ scripts/ship_data.gd
@@ -75,7 +75,8 @@ func _initialize_ship_data():
 		"Pacifica Liner",
 		"Falcon Scout",
 		"Pacer Gunship",
-		"Resolute Frigate"
+		"Resolute Frigate",
+		"Dagger Fighter"
 	]
 	
 	# Ships that need the reverse rotation (0, -90, 0) for right wingtip thrust
