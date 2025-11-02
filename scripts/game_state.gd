9dd8571dabffd7479416d8c7173eb353d85bb21e
Ariane Emory <ariane.emory@protonmail.com>
2025-11-02 13:58:44 -0500

    ...

---
+++ scripts/game_state.gd
@@ -0,0 +1,15 @@
+extends Node
+
+## Global game state manager
+## Handles game-wide state like NPC pause
+
+var npc_paused: bool = false
+
+func _input(event):
+	# Toggle NPC pause with P key
+	if event is InputEventKey and event.keycode == KEY_P and event.pressed and not event.echo:
+		toggle_npc_pause()
+
+func toggle_npc_pause():
+	npc_paused = !npc_paused
+	print("NPC ships ", "PAUSED" if npc_paused else "UNPAUSED")
